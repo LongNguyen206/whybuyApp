b1376a416e2da89ce4225f0ea7fd528998af4966
Long Nguyen <2006.long.nguyen@gmail.com>
2018-07-23 16:43:40 +1000

    Add Listings dates and photo

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,19 @@
 class Listing < ApplicationRecord
   belongs_to :user
   has_many :rent_requests, dependent: :destroy
+  validates :title, 
+            :length => { maximum: 25, too_long: "Please provide a brief title" }, 
+            :presence => true
+  validates :rate, :deposit,
+            :numericality => { greater_than_or_equal_to: 0 }
+  validates :description, :photo, :date_from,
+            :presence => true
+  validate  :end_date_after_start_date?
+
+  def end_date_after_start_date?
+    if date_to < date_from
+      errors.add :date_to, "must be after start date"
+    end
+  end
+  mount_uploader :photo, PhotoUploader
 end
